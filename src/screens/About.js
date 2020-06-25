379b5944bd8b46c02e3035f07b785dc02b44c7e0
fatihgozenc <fatihgozenc@gmail.com>
2020-06-26 00:54:34 +0200

    skill bar added

--- src/screens/About.js
+++ src/screens/About.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 export default () => {
   return (
-    <section>
-      about
+    <section className="about">
+        <div className="about__skills">
+          <div className="about__skills--bar advanced" data-skill="Javascript"/>
+          <div className="about__skills--bar expert" data-skill="SCSS"/>
+          <div className="about__skills--bar advanced" data-skill="PHP"/>
+          <div className="about__skills--bar advanced" data-skill="React"/>
+          <div className="about__skills--bar intermediate" data-skill="React Nat."/>
+          <div className="about__skills--bar advanced" data-skill="Redux"/>
+          <div className="about__skills--bar advanced" data-skill="NextJS"/>
+          <div className="about__skills--bar expert" data-skill="Wordpress"/>
+          <div className="about__skills--bar basic" data-skill="Laravel"/>
+          <div className="about__skills--bar intermediate" data-skill="Shell"/>
+          <div className="about__skills--bar intermediate" data-skill="Linux"/>
+          <div className="about__skills--bar intermediate" data-skill="Vim"/>
+          <div className="about__skills--bar learning" data-skill="Rust"/>
+        </div>
     </section>
   )
 }
