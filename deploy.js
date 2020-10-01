ab52bc18f13912e32fb8b72c9b2012237ed3f468
Fatih Gozenc <fatihgozenc@gmail.com>
2020-10-01 10:07:46 +0200

    changed animation file, searching for cssanimation bug

--- deploy.js
+++ deploy.js
@@ -3,30 +3,28 @@ const FtpDeploy = require("ftp-deploy");
 const ftpDeploy = new FtpDeploy();
 
 require('dotenv').config({
-  path: path.join(__dirname + '/.env.local')
+	path: path.join(__dirname + '/.env.local')
 })
- 
+
 const config = {
-    user: process.env.FTP_USER,
-    password: process.env.FTP_PASS,
-    host: process.env.FTP_HOST,
-    port: 21,
-    localRoot: __dirname + "/build",
-    remoteRoot: "/public_html/v3/",
-    include: ['*', '**/*'],
-    exclude: [
-      "build/**/*.map", 
-      "build/**/**/*.map", 
-      "src/*", 
-      "node_modules/**", 
-      "node_modules/**/.*", 
-      ".git/**"
-    ],
-    deleteRemote: false,
-    forcePasv: true
+	user: process.env.FTP_USER,
+	password: process.env.FTP_PASS,
+	host: process.env.FTP_HOST,
+	port: 21,
+	localRoot: __dirname + "/build",
+	remoteRoot: "/public_html/v3/",
+	include: ['*', '**/*'],
+	exclude: [
+		"src/*",
+		"node_modules/**",
+		"node_modules/**/.*",
+		".git/**"
+	],
+	deleteRemote: false,
+	forcePasv: true
 };
- 
+
 ftpDeploy
-    .deploy(config)
-    .then(res => console.log("finished:", res))
-    .catch(err => console.log(err));
+	.deploy(config)
+	.then(res => console.log("finished:", res))
+	.catch(err => console.log(err));
